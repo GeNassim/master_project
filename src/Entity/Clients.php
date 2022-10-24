b90b73a8277946990622158b09c7e52f10ce497e
GeNassim <exigezlemeilleur@gmail.com>
2022-10-24 21:56:52 +0200

    first commit

--- src/Entity/Clients.php
+++ src/Entity/Clients.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ClientsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ClientsRepository::class)]
@@ -18,7 +19,12 @@ class Clients
     #[ORM\Column(length: 255)]
     private ?string $firstname = null;
 
-    #[ORM\Column(length: 255)]
+    #[
+        ORM\Column(length: 255),
+        Assert\Email(
+            message: "{{ value }} n'est pas valide. Veuillez entrer un e-mail valide"
+        )
+    ]
     private ?string $email = null;
 
     #[ORM\ManyToOne(inversedBy: 'clients')]
